7b3dd09af26e443dedc8f2bbb1d4e3f31741b458
at89s52rc <1312788508@qq.com>
2020-08-22 09:23:49 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,116 @@
 
 
-console.log("远程脚本");
 
-$notification.post('标题内容', '子标题内容','body这部分内容') //用于通知栏提醒
+const $iosrule = iosrule();//声明必须
+
+const app = "趣看天下";
+
+
+
+if ($iosrule.isRequest) {
+
+    qktx_writeck();
+
+}
+
+$iosrule.end()
+
+
+function qktx_writeck() {
+
+
+    if ($request.headers) {
+
+        var urlVal = $request.url;
+        var headVal = $request.headers;
+        var bdVal = $request.body;
+
+        console.log(`当前抓到的url是:${urlVal}`);
+
+        if (urlval.indexOf("addCoin?") != -1) {
+
+            var ok = $iosrule.write(urlVal, "readUrl"); //阅读文章好后金币url和参数保存下来,因为是get的 
+            if (ok == true) {
+                $iosrule.notify(app, "[阅读文章]", "写入阅读文章数据成功");
+                console.log(`写入阅读文章数据成功`);
+            }
+
+        }
+        else if (urlval.indexOf("addCoinSerch?") != -1) {
+
+            var ok1 = $iosrule.write(urlVal, "searchUrl"); //因为是post的 
+            var ok2 = $iosrule.write(bdVal, "searchBody");
+            if (ok1 == true && ok2 == true) {
+                $iosrule.notify(app, "[搜索文章]", "写入搜索文章数据成功");
+                console.log(`写入搜索文章数据成功`);
+            }
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+//适合surge,圈x,loon三个通用写法
+function iosrule() {
+    const isRequest = typeof $request != "undefined"
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+    }
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => {
+                response["status"] = response.statusCode
+                callback(null, response, response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, callback)
+    }
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => {
+                response["status"] = response.statusCode
+                callback(null, response, response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.post(options, callback)
+    }
+    const end = () => {
+        if (isQuanX) isRequest ? $done({}) : ""
+        if (isSurge) isRequest ? $done({}) : $done()
+    }
+    return { isRequest, isQuanX, isSurge, notify, write, read, get, post, end }
+};
+
+
+
+
+
+
+
 
 
 
