5041e3e4cefbf0b9353f7c486f172c1ce58aeb3e
at89s52rc <1312788508@qq.com>
2020-08-26 09:35:54 +0800

    Update test_ck.js

--- test_ck.js
+++ test_ck.js
@@ -1,6 +1,6 @@
-//console.log('测试写出日志'); //输出日志
+console.log('测试写出日志'); //输出日志
 
-//$notification.post('title标题', 'subTitle子标题子标题子标题','body内容内容内容内容') //用于通知栏提醒
+$notification.post('title标题', 'subTitle子标题子标题子标题','body内容内容内容内容') //用于通知栏提醒
 
 
 
